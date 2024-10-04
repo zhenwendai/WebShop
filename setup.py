5e17604d91b5bfe49be3c9d5bd407fb88ca351b9
Zhenwen Dai <zhenwendai@gmail.com>
2024-10-04 17:42:45 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ from setuptools import setup, find_packages
 setup(
     name="webshop",
     version="0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
 )
